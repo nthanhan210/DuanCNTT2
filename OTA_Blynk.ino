9eeddc466c5cedd23e3911f4bcc0098a55e92576
nthanhan210 <100451456+nthanhan210@users.noreply.github.com>
2022-12-26 03:28:44 +0700

    Smart config+Blynk Updated
    Complete

--- OTA_Blynk.ino
+++ OTA_Blynk.ino
@@ -1,28 +1,25 @@
+#define PIN_LED 16
+#define PIN_BUTTON 0
+#define LED_ON() digitalWrite(PIN_LED, LOW)
+#define LED_OFF() digitalWrite(PIN_LED, HIGH)
+#define LED_TOGGLE() digitalWrite(PIN_LED, digitalRead(PIN_LED) ^ 0x01)
+#define BLYNK_TEMPLATE_ID "TMPLULSEfJ_m"
+#define BLYNK_DEVICE_NAME "AGV"
+#define BLYNK_AUTH_TOKEN "dorvkYcxlOdLwqXg-M_y4iKeZJEJuPOo"
+#define BLYNK_PRINT Serial
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
 #include <time.h>
-#include <Wire.h>
 #include <BlynkSimpleEsp8266.h>
-#include <EEPROM.h>
+//==================================================================================
 
-//___________Smart Config_______________
-
-#define PIN_LED 16
-#define PIN_BUTTON 0
-#define LED_ON() digitalWrite(PIN_LED, HIGH)
-#define LED_OFF() digitalWrite(PIN_LED, LOW)
-#define LED_TOGGLE() digitalWrite(PIN_LED, digitalRead(PIN_LED) ^ 0x01)
 
 Ticker ticker;
 
-String wifiSSID;
-String wifiPass;
-
-
 bool longPress()
 {
-  
   static int lastPress = 0;
   if (millis() - lastPress > 3000 && digitalRead(PIN_BUTTON) == 0) {
     return true;
@@ -48,63 +45,38 @@ void enter_smartconfig()
     WiFi.beginSmartConfig();
   }
 }
-
 void exit_smart()
 {
   ticker.detach();
-  LED_ON();
+  LED_OFF();
   in_smartconfig = false;
 }
 
+//========================================================================================
 
-
-
-//___________Blynk____________
-#define BLYNK_TEMPLATE_ID "TMPLULSEfJ_m"
-#define BLYNK_DEVICE_NAME "AGV"
-#define BLYNK_AUTH_TOKEN "dorvkYcxlOdLwqXg-M_y4iKeZJEJuPOo"
-
-#define BLYNK_PRINT Serial
 
 char auth[] = BLYNK_AUTH_TOKEN;
-
-
-
 BlynkTimer timer;
+int spd=0;
+int dir=1;
+char ssid[100];
+char pass[100];
+
 BLYNK_WRITE(V0){
-  int spd = param.asInt();
+  spd = param.asInt();
   Serial.println(spd);
-  Wire.beginTransmission(8); /* begin with device address 8 */
-  Wire.write(23);  /* sends hello slave string */
-  Wire.write(spd);
-  Wire.endTransmission();    /* stop transmitting */
  
  //Wire.requestFrom(8, 1); /* request & read data of size 13 from slave */
  
 }
 BLYNK_WRITE(V1){
-  int dir = param.asInt();
+  dir = param.asInt();
   Serial.println(dir);
-  Wire.beginTransmission(8); /* begin with device address 8 */
-  Wire.write(24);
-  Wire.write(dir);  /* sends hello slave string */
-  Wire.endTransmission();    /* stop transmitting */
  
  //Wire.requestFrom(8, 1); /* request & read data of size 13 from slave */
  
 }
-BLYNK_WRITE(V2){
-  int t15 = param.asInt();
-  Serial.println(t15);
-  Wire.beginTransmission(8); /* begin with device address 8 */
-  Wire.write(15);
-  Wire.write(t15);  /* sends hello slave string */
-  Wire.endTransmission();    /* stop transmitting */
- 
- //Wire.requestFrom(8, 1); /* request & read data of size 13 from slave */
- 
-}
-
+// This function is called every time the device is connected to the Blynk.Cloud
 BLYNK_CONNECTED()
 {
   // Change Web Link Button message to "Congratulations!"
@@ -112,122 +84,92 @@ BLYNK_CONNECTED()
   Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
   Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
 }
+
+// This function sends Arduino's uptime every second to Virtual Pin 2.
 void myTimerEvent()
 {
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V2, millis() / 1000);
+  Blynk.virtualWrite(V0, spd);
+  Blynk.virtualWrite(V1, dir);
 }
 
+//=================================================================================================
+
+
+
+
+
 void setup() {
-  Serial.begin(9600);
-  Serial.println("1");
+  Serial.begin(115200);
   Serial.setDebugOutput(true);
-  EEPROM.begin(512);
-  int add=0;
-  int startAdd=0;
-  int ssid_len = int(EEPROM.read(add));
-  add++;  
-  startAdd=add;
-  wifiSSID="";
-  for(int i=0; i<ssid_len;i++){
-    wifiSSID = wifiSSID + char(EEPROM.read(startAdd+i));
-    add++;
-  }
 
-  int pass_len = int(EEPROM.read(add));
-  add++;
-  startAdd=add;
-  wifiPass="";
-  for(int i=0; i<pass_len;i++){
-    wifiPass = wifiPass + char(EEPROM.read(startAdd+i));
-    add++;
-  }
-  char ssid[wifiSSID.length()+1];
-  char pass[wifiPass.length()+1];
-  strcpy(ssid, wifiSSID.c_str());
-  strcpy(pass, wifiPass.c_str());
-
-  Serial.print("EEP ssid: ");
-  Serial.println(wifiSSID);
-  Serial.print("EEP pass: ");
-  Serial.println(wifiPass);
-  Serial.print("length ");
-  Serial.println(wifiPass.length());
-  
-  
-  
-  //For Smart config
   pinMode(PIN_LED, OUTPUT);
   pinMode(PIN_BUTTON, INPUT);
-  ticker.attach(1, tick);
   
-  Wire.begin(D1, D2);
-  Serial.println("2");
-  //For Blynk
- 
-  // Blynk.begin(auth, ssid, pass);
-  // You can also specify server:
-  Serial.println(sizeof(ssid));
-  
-  if( wifiSSID !="" && wifiPass.length() <= 64){
-    Serial.println("3");
-    Blynk.begin(auth, ssid, pass);
-    for (int i=0; i<1024;i++){
 
-    }
-    //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
-    // Setup a function to be called every second
-    timer.setInterval(1000L, myTimerEvent);
-    Serial.println("4");
-  }
+  //WiFi.setAutoConnect(true);
+  //WiFi.setAutoReconnect(true);
+  Serial.println(WiFi.SSID());
+  Serial.println(WiFi.psk());
+  
+  Serial.printf("Connection status: %d\n", WiFi.status());
+  if(WiFi.SSID()!=""){
+    WiFi.begin();
     
-  EEPROM.end();
+    ticker.attach(0.1, tick);
+    int cntimer=millis();
+    while (WiFi.status() != WL_CONNECTED && (millis()-cntimer)<10000)
+    { 
+      delay(500);
+    }
+    if (WiFi.isConnected()){
+      
+      strcpy(ssid, WiFi.SSID().c_str());
+      strcpy(pass, WiFi.psk().c_str());
+      ticker.detach();
+      LED_OFF();
+      Serial.printf("\nConnection status: %d\n", WiFi.status());
+      Blynk.begin(auth,ssid,pass);    
+    }else {
+      ticker.attach(1, tick);
+      WiFi.disconnect();
+    }    
+  }else {
+    ticker.attach(1, tick);
+    
+  }
+  timer.setInterval(1000L, myTimerEvent);
   Serial.println("Setup done");
 }
 
-void getWifiInfo(){
-  int add=0;
-  int startAdd=0;
-  wifiSSID =WiFi.SSID();
-  wifiPass =WiFi.psk();
-
-  EEPROM.write(add,wifiSSID.length());
-  add++;
-  startAdd=add;
-  for(int i=0; i<wifiSSID.length();i++){
-    EEPROM.write(startAdd+i,wifiSSID[i]);
-    add++;
-  }
-  EEPROM.write(add,wifiPass.length());
-  add++;
-  startAdd=add;
-  for(int i=0; i<wifiPass.length();i++){
-    EEPROM.write(startAdd+i,wifiPass[i]);
-    add++;
-  }
-  EEPROM.commit();
-}
-
 void loop() {
-  if (longPress() && !Blynk.connected()) {
+  if (longPress()) {
     enter_smartconfig();
     Serial.println("Enter smartconfig");
   }
-  
   if (WiFi.status() == WL_CONNECTED && in_smartconfig && WiFi.smartConfigDone()) {
-    exit_smart();
+    exit_smart();    
+    WiFi.hostname("esp");
+    WiFi.setAutoReconnect(true);     
     Serial.println("Connected, Exit smartconfig");
-    getWifiInfo();
+    Serial.printf("Connection status: %d\n", WiFi.status());
+    Blynk.begin(auth,ssid,pass);
   }
   
-  if (WiFi.status() == WL_CONNECTED && Blynk.connected()) {
-    Blynk.run();
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println(Blynk.connected());
+    LED_ON();
     timer.run();
-    while(Wire.available()){
-      int c = Wire.read();
-      Serial.println(c);
+    if(Blynk.connected()) {
+      Blynk.run();
+    }else {
+    Blynk.connect();
     }
-    Serial.println("cncncn");
+  }else {
+    
+    if(!ticker.active()){
+      ticker.attach(1, tick);
+    }        
   }
 }
